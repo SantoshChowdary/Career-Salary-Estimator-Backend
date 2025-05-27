b41bab6e0d566907ce4b2fe7b9ed95bdbb6fafd5
Santosh <santoshssk923@gmail.com>
2025-05-28 00:42:36 +0530

    Manual Build Done

---
+++ src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const router_1 = require("./core/router");
+exports.app = (0, express_1.default)();
+exports.app.use(express_1.default.json());
+exports.app.use(express_1.default.urlencoded({ extended: true }));
+exports.app.use((0, cors_1.default)());
+exports.app.use("/user", router_1.userRouter);
+exports.app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+exports.app.listen(process.env.PORT || 3000, () => console.log("server started"));
